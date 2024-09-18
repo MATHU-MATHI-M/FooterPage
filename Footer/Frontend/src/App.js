06f61c0b52710550841c0bb77d7a04ca3e61483d
MATHU MATHI M <153843978+MATHU-MATHI-M@users.noreply.github.com>
2024-09-18 10:14:55 +0530

    Add files via upload

---
+++ Footer/Frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Footer from './components/Footer';   // Importing Footer Component
+function App() {
+  return (
+    <div>
+      {/* Other components can go here */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
